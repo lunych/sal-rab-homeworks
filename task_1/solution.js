641bcd101c9f411116795b06c41aacdf42a4808e
lunych <lun111@narod.ru>
2022-11-16 13:26:48 +0300

    task №1 done

--- task_1/solution.js
+++ task_1/solution.js
@@ -2,27 +2,28 @@ function addInCartMessage(title, price) {
     let productName = title;
     let productPrice = price;
 
-    // Задание №1.1. Оповещение о добавлении в корзину
+    let message;
 
-    // создайте переменную message
-    // присвойте переменной message значение переменной productName
-    // добавьте в конец переменной message пробел
-    // добавьте в конец переменной message текст 'за'
-    // добавьте в конец переменной message пробел
-    // добавьте в конец переменной message значение переменной productPrice
-    // добавьте в конец переменной message пробел
-    // добавьте в конец переменной message текст 'теперь в корзине!'
-    // выведите переменную message в консоль
+     message = productName;
+     message += ' ';
+     message += 'за';
+     message += ' ';
+     message += productPrice;
+     message += ' ';
+     message += 'теперь в корзине!';
 
-    // Конец решения задания №1.1.
-
-    return message;
+     console.log(message);
+     
+ return message;
 }
 
 function addInCartCountChange(value) {
 
     // Задание №1.2. Увеличение количества в корзине
     let oldValue = value;
+
+    let newValue = oldValue;
+    newValue +=  1;
 
     // создайте переменную newValue
     // присвойте ей значение oldValue
@@ -36,6 +37,11 @@ function addInCartCountChange(value) {
 function addInCartSumChange(sum, delta) {
     let oldSum = sum;
     let difference = delta;
+
+    let newSum;
+    newSum = difference + oldSum;
+    let newSumText = newSum + '₽';
+    newSumText =`${newSum} ₽`;
 
     // Задание №1.3. Увеличение суммы в корзине
 
